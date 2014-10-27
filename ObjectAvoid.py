583bf96154dbc8a98419bf0ecdcd693b378a332e
Sadman Kazi <sadmansazidk@gmail.com>
2014-10-27 18:39:07 -0400

    Added filters for the terrible sensors

--- ObjectAvoid.py
+++ ObjectAvoid.py
@@ -2,6 +2,14 @@ from myro import *
 
 initialize("com6")
 
+#To Do
+'''
+Put filter on Sensors to give better parameterization of distance
+Set turning in intervals to help with clearing the obstacle
+--> Replace 'turning time'
+Need to get final path 20 degrees from exiting straight
+'''
+
 class Robot:
     initialTurn = False
     initialTurnTime = 0
@@ -19,8 +27,8 @@ class Robot:
     driveValue = 0.5
 
     #distance sensitivity for sensors
-    sideDistance = 400
-    frontDistance = 1100
+    sideDistance = 0
+    frontDistance = 1050
 
 
     #for moving the robot the distance forward equivalent to the length of the robot
@@ -30,9 +38,17 @@ class Robot:
 
     #moves robot forward while using front sensor and stops when too close
     def moveForwardDetectFront(self):
-        values = [0, 0, 0]
-        while not(self.frontTooClose()):
+        values = [False, False, False]
+        count = 0
+        values[count] = self.frontTooClose()
+        count += 1
+        while not(values[0] and values[1] and values[2]):
             motors(self.driveValue, self.driveValue)
+            values[count] = self.frontTooClose()
+            count += 1
+
+            if count > 2:
+                count = 0
 
         #self.moveExtraBitForward()
         initialTurn = True
@@ -40,22 +56,28 @@ class Robot:
 
     #moves robot forward while using right sensor and stops when too close
     def moveForwardDetectRight(self):
-        while not(self.rightClear()):
+        values = [False, False, False, False, False]
+        count = 0
+        values[count] = self.rightClear()
+        count += 1
+
+        while not(values[0] and values[1] and values[2] and values[3] and values[4]):
             motors(self.driveValue, self.driveValue)
-            """if self.frontTooClose():
-                self.restart=True
-                stop()
-                return"""
+            values[count] = self.rightClear()
+            count += 1
+
+            if count > 4:
+                count = 0
             if self.initialTurn:
                 self.initialTurnTime+=1
         if self.initialTurn:
             initialTurn = False
-        self.moveExtraBitForward()
+        #self.moveExtraBitForward()
         stop()
 
     #move forward the original distance travelled to the left of the object
     def moveForwardSetTime(self):
-        while self.initialTurnTime*2>0:
+        while self.initialTurnTime*4>0:
             motors(self.driveValue, self.driveValue)
             self.initialTurnTime-=1
             """if self.frontTooClose():
@@ -65,13 +87,14 @@ class Robot:
 
     #detects distance from closest object in front
     def frontTooClose(self):
-        print "Front: %d\n" % getObstacle('middle')
+        print "Front: %d" % getObstacle('middle')
         if getObstacle('middle')>self.frontDistance:
             return True
 
     #detects distance from closest object to the right
     def rightClear(self):
-        if getObstacle('right')>self.sideDistance:
+        print "Right: %d" % getObstacle('right')
+        if getObstacle('right')<=self.sideDistance:
             return True
 
 def sensorTest():
