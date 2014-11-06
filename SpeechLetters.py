454c01a6437dd5c93e3fce3562e625a52e98e730
Sadman Kazi <sadmansazidk@gmail.com>
2014-11-06 12:32:28 -0500

    David's attempt at using vim

---
+++ SpeechLetters.py
@@ -0,0 +1,14 @@
+
+#This file contains the text to speech functions for the project
+
+from myro import *
+
+initialize("com6")
+
+a = "Cat"
+
+computer.saveSpeech(a, "swag.wav") 
+
+computer.playSpeech("swag.wav")
+
+
