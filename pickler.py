37143684b124861f01a56304e589e692201d9e18
Zhiyuan Wang <zhiyuan@localhost.localdomain>
2014-11-16 16:09:50 -0500

    Some minor modifications

--- pickler.py
+++ pickler.py
@@ -3,14 +3,13 @@ import pickle
 
 favorite_color = { "lion": "yellow", "kitty": "red" }
 
-pickle.dump( favorite_color, open( "save.txt", "wb" ) )
+pickle.dump( favorite_color, open( "save.p", "wb" ) )
 
 favorite_color["cat"] = "orange"
 
 print favorite_color
 
 # Load the dictionary back from the pickle file.
-import pickle
 
 favorite_color = pickle.load( open( "save.p", "rb" ) )
 # favorite_color is now { "lion": "yellow", "kitty": "red" }
