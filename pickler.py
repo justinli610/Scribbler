fa90e3a4f85512bfc6b8724a99c1410b9e78dcc1
Zhiyuan Wang <zhiyuan@localhost.localdomain>
2014-11-16 16:07:07 -0500

    Added an example of pickling in python

---
+++ pickler.py
@@ -0,0 +1,18 @@
+# Save a dictionary into a pickle file.
+import pickle
+
+favorite_color = { "lion": "yellow", "kitty": "red" }
+
+pickle.dump( favorite_color, open( "save.txt", "wb" ) )
+
+favorite_color["cat"] = "orange"
+
+print favorite_color
+
+# Load the dictionary back from the pickle file.
+import pickle
+
+favorite_color = pickle.load( open( "save.p", "rb" ) )
+# favorite_color is now { "lion": "yellow", "kitty": "red" }
+
+print favorite_color
