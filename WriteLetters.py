e7dc503b3eed3b79fc642a5f5494753d452758dc
Tammy Liu <t236liu@uwaterloo.ca>
2014-11-16 20:46:52 -0500

    Changed WriteLetters to be easier to use
    Moved alphabets to another file --> easier to edit

--- WriteLetters.py
+++ WriteLetters.py
@@ -1,6 +1,5 @@
 from myro import *
-import speech
-import time
+from WriteAlphabet import *
 
 initialize("com6")
 
@@ -27,373 +26,72 @@ def drawLetter(arg):    #include other letters
     if (arg == 'f'):
         f()
         return
-    if (arg == 't'):
-        t()
+    if (arg == 'g'):
+        g()
+        return
+    if (arg == 'h'):
+        h()
+        return
+    if (arg == 'i'):
+        i()
+        return
+    if (arg == 'j'):
+        j()
+        return
+    if (arg == 'k'):
+        k()
+        return
+    if (arg == 'l'):
+        l()
+        return
+    if (arg == 'm'):
+        m()
+        return
+    if (arg == 'n'):
+        n()
+        return
+    if (arg == 'o'):
+        o()
+        return
+    if (arg == 'p'):
+        p()
+        return
+    if (arg == 'q'):
+        q()
+        return
+    if (arg == 'r'):
+        r()
         return
     if (arg == 's'):
         s()
         return
+    if (arg == 't'):
+        t()
+        return
+    if (arg == 'u'):
+        u()
+        return
+    if (arg == 'v'):
+        v()
+        return
+    if (arg == 'w'):
+        w()
+        return
+    if (arg == 'x'):
+        x()
+        return
+    if (arg == 'y'):
+        y()
+        return
+    if (arg == 'z'):
+        z()
+        return
+    if (arg == ' '):
+        space()
+        space()
+        return
     return
-'''
-move(translate, rotate)
-First param: 1, forward, -1, backwards, fullspeed
-Second param: 1, left, -1, right, fullspeed
-'''
-'''def arc(degrees,radius = 1):    #may need to change function to implement different radii
-    print ("Arc: " + str(degrees))
-    #speed = 0.2
-    #moveTime = 0.035;#Tweek this value so that it turns the correct amount
-    speed = 0.3
-    moveTime = 0.0232
-    if (degrees >= 0):
-        move(speed,speed/radius)       #try changing to motors(left speed?, right speed?) +stop()
-    else:
-        move(speed,-speed/radius)
-    print(abs(degrees)*moveTime)
-    wait(abs(degrees)*moveTime)
-    move(0,0)
-    wait(0.3)#Let the robot wheels recover
-'''
-def line(length):
-    print ("Line: " + str(length))
-    #speed = 0.3
-    #moveTime = 0.75#Tweek this value so that it turns the correct amount
-    speed = 0.5
-    moveTime = 0.5
-    if (length >= 0):
-        forward(speed,length*moveTime)
-    else:
-        backward(speed,abs(length)*moveTime)
-    wait(0.3)#Let the robot wheels recover
-
-def turn(degrees):
-    print ("Turn: " + str(degrees))
-    #speed = 0.2
-    #turnTime = 0.0348;
-    speed = 0.3
-    turnTime = 0.0255
-    if degrees > 0:#Left turn, CCW
-        turnLeft(speed, degrees*turnTime)
-    else:#Right turn, CW
-        turnRight(speed, abs(degrees)*turnTime)
-    wait(0.3)#Let the robot wheels recover
-
-#Small turn (e.g. for 's')
-def arc(degrees):
-    print ("Turn: " + str(degrees))
-    #speed = 0.2
-    #turnTime = 0.0348;
-    speed = 0.2
-    turnPower = 0.4 *degrees/abs(degrees)
-    move(speed, turnPower)
-    """if degrees > 0:#Left turn, CCW
-        turnPower = degrees*time
-    else:#Right turn, CW
-        turnPower = abs(degrees)*time"""
-
-    while timeRemaining(.0165*abs(degrees)):
-        i = 0
-    move(0, 0)
-    wait(0.3)#Let the robot wheels recover
-
-'''
-Always move line(3) after drawing the letter to make space for the next letter
-'''
-
-#Draws an a
-#Also includes the return movements to the baseline
-def a():
-    space()#To clear more space for the circle
-    arc(360)#Break up the movements because bug with angles > 400
-    arc(90)
-    turn(180)
-    line(2)
-    turn(90)
-    space()
-
-def b():
-    turn(90)
-    line(4)
-    turn(180)
-    line(3)
-    arc(360 + 30)
-    line(1)
-    turn(90)
-    space()
-
-def c():
-    turn(180)
-    arc(-180)
-    turn(180)
-    arc(180)
-    space()
-
-def d():
-    space()
-    arc(360+30)
-    arc(90)
-    line(3)
-    turn(180)
-    line(4)
-    turn(90)
-    space()
-
-def e():    #finetune
-    space()
-    turn(180)
-    arc(-270-10)
-    turn(-90)
-    line(1.8)
-    turn(90)
-    arc(140)
-    turn(180)
-    arc(-40)
-    turn(180)
-    space()
-
-def f():
-    turn(90)
-    line(3)
-    arc(-90)
-    turn(180)
-    arc(90)
-    line(0.7)
-    turn(90)
-    line(0.9)
-    line(-0.6)
-    line(-0.7)
-    line(0.7)
-    turn(-90)
-    line(2.3)
-    turn(90)
-    space()
-def g():
-    line(3)
-    arc(360 + 30)
-    arc(90)
-    line(0.7)
-    turn(180)
-    line(3)
-    arc(-180)
-    turn(180)
-    arc(180+20)
-    line(1.4)
-    turn(-90)
-    line(3)
-def h():
-    turn(90)
-    line(4)
-    line(-3)
-    arc(-180)
-    line(1)
-    turn(90)
-    space()
-def i():#test
-    turn(90)
-    line(1)
-    turn(-90)
-    arc(360,0.5)    #change function to use different radii
-    turn(-90)
-    line(3)
-    turn(90)
-    space()
-def j():#test
-    turn(90)
-    line(3)
-    turn(-90)
-    arc(360,0.5)    #change function to use different radii
-    turn(-90)
-    line(5)
-    arc(-180)
-    turn(180)
-    arc(180)
-    line(2)
-    turn(-90)
-    line(3)
-def k():
-    turn(90)
-    line(3)
-    line(-2)
-    turn(-45)
-    line(2)
-    line(-1.5)
-    turn(-90)
-    line(1.5)
-    turn(45)
-    space()
-def l():
-    turn(90)
-    line(3)
-    line(-3)
-    turn(-90)
-    space()
-def m():#finetune
-    turn(90)
-    line(1.5)
-    line(-0.5)
-    arc(-180)
-    line(1)
-    turn(180)
-    line(1)
-    arc(-180)
-    line(1)
-    turn(90)
-    space()
-def n():#finetune
-    turn(90)
-    line(1.5)
-    line(-0.5)
-    arc(-180)
-    line(1)
-    turn(90)
-    space()
-def o():#test
-    line(3)
-    arc(360)
-    line(3)
-def p():
-    turn(-90)
-    line(2)
-    line(-5)
-    line(1)
-    arc(360)
-    line(2)
-    turn(90)
-    line(3)
-def q():#test
-    line(3)
-    arc(360)
-    arc(90)
-    line(2)
-    line(-5)
-    turn(-45)
-    line(2)
-    line(-2)
-    turn(45)
-    line(2)
-    turn(-90)
-    line(3)
-def r():#test
-    turn(90)
-    line(3)
-    line(-1)
-    arc(-180)
-    turn(180)
-    arc(180)
-    line(2)
-    turn(90)
-    line(3)
-def s():
-    line(3)
-    turn(180)
-    arc(-90)
-    turn(180)
-    arc(270)
-    arc(-260)
-    turn(180)
-    arc(260)
-    arc(-180)
-    turn(180)
-    line(3)
-def t():
-    turn(90)
-    line(5)
-    line(-1)
-    turn(90)
-    line(1)
-    line(-2)
-    line(1)
-    turn(90)
-    line(4)
-    turn(90)
-    line(3)
-def u():#test
-    line(3)
-    turn(180)
-    arc(-90)
-    line(2)
-    turn(180)
-    line(2)
-    arc(180)
-    line(2)
-    turn(180)
-    line(3)
-    turn(90)
-    line(3)
-def v():#test
-    line(3)
-    turn(110)
-    line(2)
-    line(-2)
-    turn(-40)
-    line(2)
-    line(-2)
-    turn(-70)
-    line(3)
-def w():#test
-    line(3)
-    turn(110)
-    line(2)
-    line(-2)
-    turn(-40)
-    line(2)
-    turn(-140)
-    line(2)
-    turn(140)
-    line(2)
-    line(-2)
-    turn(-70)
-    line(3)
-def x():#test
-    turn(60)
-    line(2)
-    line(-1)
-    turn(40)
-    line(1)
-    line(-2)
-    turn(-110)
-    line(3)
-def y():#test
-    line(3)
-    turn(110)
-    line(2)
-    line(-2)
-    turn(-40)
-    line(2)
-    line(-4)
-    line(2)
-    turn(-60)
-    line(3)
-def z():#test
-    line(3)
-    turn(90)
-    line(0.5)
-    turn(90)
-    line(1.5)   #might be weird
-    turn(-135)
-    line(2)
-    turn(135)
-    line(1.5)
-    line(-1.5)
-    turn(45)
-    line(2)
-    turn(135)
-    line(1.5)
-    turn(-90)
-    line(0.5)
-    turn(90)
-    line(3)
-def space():
-    line(2)
-
-m()
-n()
-#arc(360)
-#turn(360)
-#a()
-#c()
-#t()
-#drawWord("e")
+drawWord("a dog")
 '''
 phrase = speech.input()
 print phrase
